aedcb388284d969edc045e09b4da83f3ed68a907
pjg09 <pedroj1229@gmail.com>
2026-02-10 18:23:34 -0500

    chore: Spring Boot set up

---
+++ src/main/java/com/upb/examendevops/ExamenDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.upb.examendevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenDevOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upb/examendevops/ExamenDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upb.examendevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
